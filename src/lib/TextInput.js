c49aad8fbbe7135c5d235358b1a5971247f8647e
brandon <leeyongl5263@gmail.com>
2020-02-24 14:49:53 -0800

    Ads

--- src/lib/TextInput.js
+++ src/lib/TextInput.js
@@ -4,6 +4,7 @@ import "./TextInput.css";
 const TextInput = ({ type = "text", label, value, onChange }) => (
   <div className="simple-form-group">
     {label && <label className="simple-text-label">{label}</label>}
+    
     <input
       type={type}
       className="simple-text-input"
