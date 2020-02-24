53c33f15d9e3e59a484c5de53c8dace9c33376ef
brandon <leeyongl5263@gmail.com>
2020-02-24 15:57:49 -0800

    test

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = ({ type = "text", label, value, onChange }) => (
+  <div className="simple-form-group">
+    <button style={{ backgroundColor: "white"}}> Test </button>
+  </div>
+);
+
+export default Button;
