8a02578f3a5c776df097c77b669915cd44184b6b
brandon <leeyongl5263@gmail.com>
2020-02-25 12:51:13 -0800

    TEST

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+module.exports = function BabelConfigJS(api) {
+    api.cache(true);
+
+    const presets = ["@babel/preset-env", "@babel/preset-react"];
+
+    const plugins = [
+        "styled-components",
+        [
+            "@babel/plugin-transform-runtime",
+            {
+                corejs: 2,
+                helpers: true,
+                regenerator: true,
+                useESModules: false,
+            },
+        ],
+        [
+            "@babel/plugin-proposal-class-properties",
+            {
+                loose: true,
+            },
+        ],
+    ];
+
+    return {
+        sourceType: "unambiguous",
+        presets,
+        plugins,
+    };
+};
