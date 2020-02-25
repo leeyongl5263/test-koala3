e0b3947959cc12417a04b05f548ab2e99de7b38c
brandon <leeyongl5263@gmail.com>
2020-02-24 16:02:37 -0800

    Testing

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,12 @@
 import React from "react";
+import ButtonWithStyle from './Button.styles';
 
 const Button = ({ type = "text", label, value, onChange }) => (
   <div className="simple-form-group">
-    <button style={{ backgroundColor: "white"}}> Test </button>
+    {/* <button style={{ backgroundColor: "white"}}> Test </button> */}
+    <ButtonWithStyle ref={ref} {...props} >
+      {props.children}
+    </ButtonWithStyle>
   </div>
 );
 
