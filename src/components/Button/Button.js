affafacb336f2427e5ea4497d5b690db85f99c3e
brandon <leeyongl5263@gmail.com>
2020-02-24 16:28:19 -0800

    Test

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,28 @@
 import React from "react";
 import styled from 'styled-components';
 
-const Button = ({ type = "text", label, value, onChange }) => (
-  <div className="simple-form-group">
-    {styled.button``}
-    {/* <button style={{ backgroundColor: "white"}}> Test </button> */}
-  </div>
-);
+// const Button = ({ type = "text", label, value, onChange }) => (
+//   <div className="simple-form-group">
+//     {styled.button``}
+//     {/* <button style={{ backgroundColor: "white"}}> Test </button> */}
+//   </div>
+// );
 
-export default Button;
+// export default Button;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
+// const Container = styled.div`
+//   text-align: center;
+// `
+export { Button }
