e1ef926c336824a4611b5b0344c9a2ae162e5c4c
brandon <leeyongl5263@gmail.com>
2020-02-25 14:24:54 -0800

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist/'),
         publicPath: '',
-        filename: 'build.js',
+        filename: 'index.js',
         libraryTarget: 'umd'
     }
 };
