1de49d3524728aefc983bf04dfb9bf982d449767
brandon <leeyongl5263@gmail.com>
2020-02-25 16:35:02 -0800

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -19,3 +19,42 @@ module.exports = {
         libraryTarget: 'umd'
     }
 };
+
+module.exports = {  
+    output: {path: path.resolve(__dirname, 'dist/'),
+    publicPath: '',
+    filename: 'index.js',
+    libraryTarget: 'umd'},  
+    plugins: {...},  
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use:{
+                    loader: "babel-loader",
+                },
+            },
+        ]
+    }, 
+    resolve: {      
+        alias: {          
+            'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
+        }  
+    },  
+    externals: {      
+        // Don't bundle react or react-dom      
+        react: {          
+            commonjs: "react",          
+            commonjs2: "react",          
+            amd: "React",          
+            root: "React"      
+        },      
+        "react-dom": {          
+            commonjs: "react-dom",          
+            commonjs2: "react-dom",          
+            amd: "ReactDOM",          
+            root: "ReactDOM"      
+        }  
+    } 
+}
